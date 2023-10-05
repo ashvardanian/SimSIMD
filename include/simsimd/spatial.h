1f2dc65ff2fb4dffff6b555bc6e5ab7cbb54fe11
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-10-05 00:04:12 -0700

    Make: Use `arch=armv8.2-a+dotprod` flag

--- include/simsimd/spatial.h
+++ include/simsimd/spatial.h
@@ -272,7 +272,7 @@ simsimd_neon_i8_l2sq(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_
     return d2;
 }
 
-__attribute__((target("+simd+dotprod"))) //
+__attribute__((target("arch=armv8.2-a+dotprod"))) //
 inline static simsimd_f32_t
 simsimd_neon_i8_cos(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t d) {
 
@@ -282,26 +282,26 @@ simsimd_neon_i8_cos(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t
     simsimd_size_t i = 0;
 
     // If the 128-bit `vdot_s32` intrinsic is unavailable, we can use the 64-bit `vdot_s32`.
-    for (simsimd_size_t i = 0; i != d; i += 8) {
-        int16x8_t a_vec = vmovl_s8(vld1_s8(a + i));
-        int16x8_t b_vec = vmovl_s8(vld1_s8(b + i));
-        int16x8_t ab_part_vec = vmulq_s16(a_vec, b_vec);
-        int16x8_t a2_part_vec = vmulq_s16(a_vec, a_vec);
-        int16x8_t b2_part_vec = vmulq_s16(b_vec, b_vec);
-        ab_vec = vaddq_s32(ab_vec, vaddq_s32(vmovl_s16(vget_high_s16(ab_part_vec)), //
-                                             vmovl_s16(vget_low_s16(ab_part_vec))));
-        a2_vec = vaddq_s32(a2_vec, vaddq_s32(vmovl_s16(vget_high_s16(a2_part_vec)), //
-                                             vmovl_s16(vget_low_s16(a2_part_vec))));
-        b2_vec = vaddq_s32(b2_vec, vaddq_s32(vmovl_s16(vget_high_s16(b2_part_vec)), //
-                                             vmovl_s16(vget_low_s16(b2_part_vec))));
-    }
-    // for (; i + 15 < d; i += 16) {
-    //     int8x16_t a_vec = vld1q_s8(a + i);
-    //     int8x16_t b_vec = vld1q_s8(b + i);
-    //     ab_vec = vdotq_s32(ab_vec, a_vec, b_vec);
-    //     a2_vec = vdotq_s32(a2_vec, a_vec, a_vec);
-    //     b2_vec = vdotq_s32(b2_vec, b_vec, b_vec);
+    // for (simsimd_size_t i = 0; i != d; i += 8) {
+    //     int16x8_t a_vec = vmovl_s8(vld1_s8(a + i));
+    //     int16x8_t b_vec = vmovl_s8(vld1_s8(b + i));
+    //     int16x8_t ab_part_vec = vmulq_s16(a_vec, b_vec);
+    //     int16x8_t a2_part_vec = vmulq_s16(a_vec, a_vec);
+    //     int16x8_t b2_part_vec = vmulq_s16(b_vec, b_vec);
+    //     ab_vec = vaddq_s32(ab_vec, vaddq_s32(vmovl_s16(vget_high_s16(ab_part_vec)), //
+    //                                          vmovl_s16(vget_low_s16(ab_part_vec))));
+    //     a2_vec = vaddq_s32(a2_vec, vaddq_s32(vmovl_s16(vget_high_s16(a2_part_vec)), //
+    //                                          vmovl_s16(vget_low_s16(a2_part_vec))));
+    //     b2_vec = vaddq_s32(b2_vec, vaddq_s32(vmovl_s16(vget_high_s16(b2_part_vec)), //
+    //                                          vmovl_s16(vget_low_s16(b2_part_vec))));
     // }
+    for (; i + 15 < d; i += 16) {
+        int8x16_t a_vec = vld1q_s8(a + i);
+        int8x16_t b_vec = vld1q_s8(b + i);
+        ab_vec = vdotq_s32(ab_vec, a_vec, b_vec);
+        a2_vec = vdotq_s32(a2_vec, a_vec, a_vec);
+        b2_vec = vdotq_s32(b2_vec, b_vec, b_vec);
+    }
 
     int32_t ab = vaddvq_s32(ab_vec);
     int32_t a2 = vaddvq_s32(a2_vec);
@@ -321,7 +321,7 @@ simsimd_neon_i8_cos(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t
     return 1 - ab * a2_b2_arr[0] * a2_b2_arr[1];
 }
 
-__attribute__((target("+simd+dotprod"))) //
+__attribute__((target("arch=armv8.2-a+dotprod"))) //
 inline static simsimd_f32_t
 simsimd_neon_i8_ip(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t d) {
     return simsimd_neon_i8_cos(a, b, d);
