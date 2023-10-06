8a2b2d7196b89f254c945ed3961be5a980d3d45c
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-10-06 20:40:13 +0000

    Add: CLI arguments to benchmarks

--- python/bench.py
+++ python/bench.py
@@ -1,7 +1,22 @@
+import timeit
+import argparse
+
 import numpy as np
 import scipy.spatial.distance as spd
 import simsimd as simd
-import timeit
+
+# Argument parsing
+parser = argparse.ArgumentParser(description="Benchmark SimSIMD vs. SciPy")
+parser.add_argument(
+    "--n", type=int, default=1000, help="Number of vectors (default: 1000)"
+)
+parser.add_argument(
+    "--ndim", type=int, default=1536, help="Number of dimensions (default: 1536)"
+)
+args = parser.parse_args()
+
+count = args.n
+ndim = args.ndim
 
 
 # Benchmark function
@@ -24,16 +39,42 @@ print()
 print("# Benchmarking SimSIMD vs. SciPy")
 print()
 
-print("Enabled capabilities:")
-print()
-for capability, enabled in simd.get_capabilities().items():
-    print(f"- {capability}: {enabled}")
+print("- Vector dimensions:", ndim)
+print("- Vectors count:", count)
+
+try:
+    caps = [cap for cap, enabled in simd.get_capabilities().items() if enabled]
+    print("- Hardware capabilities:", ", ".join(caps))
+
+    deps: dict = np.show_config(mode="dicts").get("Build Dependencies")
+    print("- NumPy BLAS dependency:", deps["blas"]["name"])
+    print("- NumPy LAPACK dependency:", deps["lapack"]["name"])
+
+except:
+    pass
 
 count = 1000
 ndim = 1536
 
+generators = {
+    np.float32: lambda: np.random.randn(count, ndim).astype(np.float32),
+    np.float16: lambda: np.random.randn(count, ndim).astype(np.float16),
+    np.int8: lambda: np.random.randint(-100, 100, (count, ndim), np.int8),
+    np.uint8: lambda: np.packbits(
+        np.random.randint(0, 2, (count, ndim), np.uint8), axis=0
+    ),
+}
+
+dtype_names = {
+    np.float32: "f32",
+    np.float16: "f16",
+    np.int8: "i8",
+    np.uint8: "u8",
+}
+
+
 print()
-print("## Between 2 Vectors, Batch Size: {:,}".format(count))
+print("## Between 2 Vectors, Batch Size: 1")
 print()
 
 # Table headers
@@ -58,21 +99,52 @@ funcs = [
     ("scipy.jaccard", spd.jaccard, simd.jaccard, [np.uint8]),
 ]
 
-generators = {
-    np.float32: lambda: np.random.randn(count, ndim).astype(np.float32),
-    np.float16: lambda: np.random.randn(count, ndim).astype(np.float16),
-    np.int8: lambda: np.random.randint(-100, 100, (count, ndim), np.int8),
-    np.uint8: lambda: np.packbits(
-        np.random.randint(0, 2, (count, ndim), np.uint8), axis=0
-    ),
-}
 
-dtype_names = {
-    np.float32: "f32",
-    np.float16: "f16",
-    np.int8: "i8",
-    np.uint8: "u8",
-}
+for name, conventional_func, simd_func, dtypes in funcs:
+    for dtype in dtypes:
+        A = generators[dtype]()
+        B = generators[dtype]()
+
+        conventional_time = benchmark(wrap_rowwise(conventional_func), A, B, count)
+        simd_time = benchmark(wrap_rowwise(simd_func), A, B, count)
+
+        conventional_ops = 1 / conventional_time
+        simd_ops = 1 / simd_time
+        improvement = conventional_time / simd_time
+
+        func_name = "`{}`".format(name)
+        dtype_name = "`{}`".format(dtype_names[dtype])
+        print(
+            f"| {dtype_name:8} | {func_name:21} | {conventional_ops:20,.0f} | {simd_ops:20,.0f} | {improvement:17.2f} x |"
+        )
+
+print()
+
+
+print("## Between 2 Vectors, Batch Size: {:,}".format(count))
+print()
+
+# Table headers
+print(
+    "| Datatype | Method                |                Ops/s |        SimSIMD Ops/s | SimSIMD Improvement |"
+)
+print(
+    "| :------- | :-------------------- | -------------------: | -------------------: | ------------------: |"
+)
+
+# Benchmark functions
+funcs = [
+    ("scipy.cosine", spd.cosine, simd.cosine, [np.float32, np.float16, np.int8]),
+    (
+        "scipy.sqeuclidean",
+        spd.sqeuclidean,
+        simd.sqeuclidean,
+        [np.float32, np.float16, np.int8],
+    ),
+    ("numpy.inner", np.inner, simd.inner, [np.float32, np.float16, np.int8]),
+    ("scipy.hamming", spd.hamming, simd.hamming, [np.uint8]),
+    ("scipy.jaccard", spd.jaccard, simd.jaccard, [np.uint8]),
+]
 
 
 for name, conventional_func, simd_func, dtypes in funcs:
