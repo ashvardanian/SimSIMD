dc6de111291bf125c9b4fd9c1cfc85cb06e1188b
Steven Roussey <sroussey@gmail.com>
2024-01-22 10:11:30 -0800

    Fix: fallback for Vercel-based apps (#66)
    * Fallback must have same signature
    * All remaining fallback implementations and add associated tests
    * Rename "inner product" to distance in docs

--- javascript/fallback.ts
+++ javascript/fallback.ts
@@ -1,51 +1,53 @@
-console.warn(
-  "It seems like your environment does't support the native simsimd module, so we are providing a JS fallback."
-);
-
 /**
- * Computes the inner distance between two arrays.
- * @param {number[]} arr1 - The first array.
- * @param {number[]} arr2 - The second array.
- * @returns {number} The inner distance between arr1 and arr2.
+ * @brief Computes the inner distance of two vectors.
+ * @param {Float32Array} a - The first vector.
+ * @param {Float32Array} b - The second vector.
+ * @returns {number} The inner distance of vectors a and b.
  */
-export function inner_distance(arr1: number[], arr2: number[]): number {
-  if (arr1.length !== arr2.length) {
+export function inner(a: Float32Array, b: Float32Array): number {
+  if (a.length !== b.length) {
     throw new Error("Vectors must have the same length");
   }
 
   let result = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    result += arr1[i] * arr2[i];
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
   }
   return 1 - result;
 }
 
 /**
- * Computes the squared Euclidean distance between two arrays.
- * @param {number[]} arr1 - The first array.
- * @param {number[]} arr2 - The second array.
- * @returns {number} The squared Euclidean distance between arr1 and arr2.
+ * @brief Computes the squared Euclidean distance between two vectors.
+ * @param {Float32Array|Int8Array} a - The first vector.
+ * @param {Float32Array|Int8Array} b - The second vector.
+ * @returns {number} The squared Euclidean distance between vectors a and b.
  */
-export function sqeuclidean(arr1: number[], arr2: number[]): number {
-  if (arr1.length !== arr2.length) {
+export function sqeuclidean(
+  a: Float32Array | Int8Array,
+  b: Float32Array | Int8Array
+): number {
+  if (a.length !== b.length) {
     throw new Error("Vectors must have the same length");
   }
 
   let result = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    result += (arr1[i] - arr2[i]) * (arr1[i] - arr2[i]);
+  for (let i = 0; i < a.length; i++) {
+    result += (a[i] - b[i]) * (a[i] - b[i]);
   }
   return result;
 }
 
 /**
- * Computes the cosine distance between two arrays.
- * @param {number[]} arr1 - The first array.
- * @param {number[]} arr2 - The second array.
- * @returns {number} The cosine distance between arr1 and arr2.
+ * @brief Computes the cosine distance between two vectors.
+ * @param {Float32Array|Int8Array} a - The first vector.
+ * @param {Float32Array|Int8Array} b - The second vector.
+ * @returns {number} The cosine distance between vectors a and b.
  */
-export function cosine(arr1: number[], arr2: number[]): number {
-  if (arr1.length !== arr2.length) {
+export function cosine(
+  a: Float32Array | Int8Array,
+  b: Float32Array | Int8Array
+): number {
+  if (a.length !== b.length) {
     throw new Error("Vectors must have the same length");
   }
 
@@ -53,10 +55,10 @@ export function cosine(arr1: number[], arr2: number[]): number {
   let magnitudeA = 0;
   let magnitudeB = 0;
 
-  for (let i = 0; i < arr1.length; i++) {
-    dotProduct += arr1[i] * arr2[i];
-    magnitudeA += arr1[i] * arr1[i];
-    magnitudeB += arr2[i] * arr2[i];
+  for (let i = 0; i < a.length; i++) {
+    dotProduct += a[i] * b[i];
+    magnitudeA += a[i] * a[i];
+    magnitudeB += b[i] * b[i];
   }
 
   magnitudeA = Math.sqrt(magnitudeA);
@@ -71,3 +73,121 @@ export function cosine(arr1: number[], arr2: number[]): number {
 
   return 1 - dotProduct / (magnitudeA * magnitudeB);
 }
+
+/**
+ * @brief Computes the bitwise Hamming distance between two vectors.
+ * @param {Uint8Array} a - The first vector.
+ * @param {Uint8Array} b - The second vector.
+ * @returns {number} The Hamming distance between vectors a and b.
+ */
+export const hamming = (a: Uint8Array, b: Uint8Array): number => {
+  if (a.length !== b.length) {
+    throw new Error("Arrays must be of the same length");
+  }
+
+  let distance = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    let xor = a[i] ^ b[i]; // XOR operation to find differing bits
+
+    // Count the number of set bits (differing bits)
+    while (xor > 0) {
+      distance += xor & 1;
+      xor >>= 1;
+    }
+  }
+
+  return distance;
+};
+
+/**
+ * @brief Computes the bitwise Jaccard similarity coefficient between two vectors.
+ * @param {Uint8Array} a - The first vector.
+ * @param {Uint8Array} b - The second vector.
+ * @returns {number} The Jaccard similarity coefficient between vectors a and b.
+ */
+export const jaccard = (a: Uint8Array, b: Uint8Array): number => {
+  if (a.length !== b.length) {
+    throw new Error("Arrays must be of the same length");
+  }
+
+  let intersection = 0;
+  let union = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    let ai = a[i];
+    let bi = b[i];
+
+    // Count the number of set bits in a AND b for intersection
+    let and = ai & bi;
+    while (and > 0) {
+      intersection += and & 1;
+      and >>= 1;
+    }
+
+    // Count the number of set bits in a OR b for union
+    let or = ai | bi;
+    while (or > 0) {
+      union += or & 1;
+      or >>= 1;
+    }
+  }
+
+  if (union === 0) return 0; // Avoid division by zero
+
+  return 1 - intersection / union;
+};
+
+/**
+ * @brief Computes the kullbackleibler similarity coefficient between two vectors.
+ * @param {Float32Array} a - The first vector.
+ * @param {Float32Array} b - The second vector.
+ * @returns {number} The Jaccard similarity coefficient between vectors a and b.
+ */
+export const kullbackleibler = (a: Float32Array, b: Float32Array): number => {
+  if (a.length !== b.length) {
+    throw new Error("Arrays must be of the same length");
+  }
+
+  let divergence = 0.0;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+      if (b[i] === 0) {
+        throw new Error(
+          "Division by zero encountered in KL divergence calculation"
+        );
+      }
+      divergence += a[i] * Math.log(a[i] / b[i]);
+    }
+  }
+
+  return divergence;
+};
+
+/**
+ * @brief Computes the jensenshannon similarity coefficient between two vectors.
+ * @param {Float32Array} a - The first vector.
+ * @param {Float32Array} b - The second vector.
+ * @returns {number} The Jaccard similarity coefficient between vectors a and b.
+ */
+export const jensenshannon = (p: Float32Array, q: Float32Array): number => {
+  if (p.length !== q.length) {
+    throw new Error("Arrays must be of the same length");
+  }
+
+  const m = p.map((value, index) => (value + q[index]) / 2);
+
+  const divergence = 0.5 * kullbackleibler(p, m) + 0.5 * kullbackleibler(q, m);
+  return Math.sqrt(divergence);
+};
+
+export default {
+  sqeuclidean,
+  cosine,
+  inner,
+  hamming,
+  jaccard,
+  kullbackleibler,
+  jensenshannon,
+};
