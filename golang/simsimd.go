78785330cfb0770d2141f6f177e308f65b9f5669
Daniel Bos <daniel.bos@sap.com>
2024-01-24 17:22:23 +0800

    golang: correct data type in function comments
    Update the comments for CosineF32, InnerF32, and SqEuclideanF32
    functions in the simsimd.go file. The comments incorrectly stated
    that these functions compute the similarity between two i8 vectors.
    They actually compute the similarity between two f32 vectors. This
    change ensures that the comments accurately reflect the
    functionality of the code.

--- golang/simsimd.go
+++ golang/simsimd.go
@@ -15,13 +15,12 @@ inline static simsimd_f32_t sqeuclidean_f32(simsimd_f32_t const* a, simsimd_f32_
 */
 import "C"
 
-
 // CosineI8 computes the cosine distance between two i8 vectors using the most suitable SIMD instruction set available.
 func CosineI8(a, b []int8) float32 {
 	return float32(C.cosine_i8((*C.simsimd_i8_t)(&a[0]), (*C.simsimd_i8_t)(&b[0]), C.simsimd_size_t(len(a))))
 }
 
-// CosineF32 computes the cosine distance between two i8 vectors using the most suitable SIMD instruction set available.
+// CosineF32 computes the cosine distance between two f32 vectors using the most suitable SIMD instruction set available.
 func CosineF32(a, b []float32) float32 {
 	return float32(C.cosine_f32((*C.simsimd_f32_t)(&a[0]), (*C.simsimd_f32_t)(&b[0]), C.simsimd_size_t(len(a))))
 }
@@ -31,7 +30,7 @@ func InnerI8(a, b []int8) float32 {
 	return float32(C.inner_i8((*C.simsimd_i8_t)(&a[0]), (*C.simsimd_i8_t)(&b[0]), C.simsimd_size_t(len(a))))
 }
 
-// InnerF32 computes the inner-product similarity between two i8 vectors using the most suitable SIMD instruction set available.
+// InnerF32 computes the inner-product similarity between two f32 vectors using the most suitable SIMD instruction set available.
 func InnerF32(a, b []float32) float32 {
 	return float32(C.inner_f32((*C.simsimd_f32_t)(&a[0]), (*C.simsimd_f32_t)(&b[0]), C.simsimd_size_t(len(a))))
 }
@@ -41,7 +40,7 @@ func SqEuclideanI8(a, b []int8) float32 {
 	return float32(C.sqeuclidean_i8((*C.simsimd_i8_t)(&a[0]), (*C.simsimd_i8_t)(&b[0]), C.simsimd_size_t(len(a))))
 }
 
-// SqEuclideanF32 computes the squared euclidean similarity between two i8 vectors using the most suitable SIMD instruction set available.
+// SqEuclideanF32 computes the squared euclidean similarity between two f32 vectors using the most suitable SIMD instruction set available.
 func SqEuclideanF32(a, b []float32) float32 {
 	return float32(C.sqeuclidean_f32((*C.simsimd_f32_t)(&a[0]), (*C.simsimd_f32_t)(&b[0]), C.simsimd_size_t(len(a))))
 }
