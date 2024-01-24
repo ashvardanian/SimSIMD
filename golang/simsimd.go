80cbe51a005eaaaa3f699fd094061aae82527364
Daniel Bos <daniel.bos@sap.com>
2024-01-24 18:03:12 +0800

    golang: update cosine_f32 function in simsimd.go
    The cosine_f32 function in simsimd.go has been updated to use
    simsimd_metric_punned instead of simsimd_neon_f32_cos. This change was
    necessary to ensure compatibility across different SIMD architectures.
    The previous implementation was specific to NEON, which could cause
    issues on systems that do not support it.
    
    This resolves the error:
    
    ```
    ./simsimd.go: In function 'cosine_f32':
    ./simsimd.go:10:115: warning: implicit declaration of function 'simsimd_neon_f32_cos'; did you mean 'simsimd_serial_f32_cos'?  [-Wimplicit-function-declaration]
    ```

--- golang/simsimd.go
+++ golang/simsimd.go
@@ -7,7 +7,7 @@ package simsimd
 #include <stdlib.h>
 
 inline static simsimd_f32_t cosine_i8(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t d) { return simsimd_metric_punned(simsimd_metric_cosine_k, simsimd_datatype_i8_k, simsimd_cap_any_k)(a, b, d, d); }
-inline static simsimd_f32_t cosine_f32(simsimd_f32_t const* a, simsimd_f32_t const* b, simsimd_size_t d) { return simsimd_neon_f32_cos(a, b, d); }
+inline static simsimd_f32_t cosine_f32(simsimd_f32_t const* a, simsimd_f32_t const* b, simsimd_size_t d) { return simsimd_metric_punned(simsimd_metric_cosine_k, simsimd_datatype_f32_k, simsimd_cap_any_k)(a, b, d, d); }
 inline static simsimd_f32_t inner_i8(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t d) { return simsimd_metric_punned(simsimd_metric_inner_k, simsimd_datatype_i8_k, simsimd_cap_any_k)(a, b, d, d); }
 inline static simsimd_f32_t inner_f32(simsimd_f32_t const* a, simsimd_f32_t const* b, simsimd_size_t d) { return simsimd_metric_punned(simsimd_metric_inner_k, simsimd_datatype_f32_k, simsimd_cap_any_k)(a, b, d, d); }
 inline static simsimd_f32_t sqeuclidean_i8(simsimd_i8_t const* a, simsimd_i8_t const* b, simsimd_size_t d) { return simsimd_metric_punned(simsimd_metric_sqeuclidean_k, simsimd_datatype_i8_k, simsimd_cap_any_k)(a, b, d, d); }
