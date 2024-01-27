461d67c6b30cd0eae1fe4e02a59909d294f78118
Pedro Gabriel <pedro.planel@2gofintech.com.br>
2024-01-27 04:22:57 -0300

    fix: detect if build is running on Docs.rs builder machine
    Our Docs.rs builds are failing because SimSIMD's _Float16 type
    is not supported on their builder.
    ```
    [INFO] [stderr] warning: simsimd@0.1.0: In file included from rust/lib.c:1:
    [INFO] [stderr] warning: simsimd@0.1.0: In file included from include/simsimd/simsimd.h:21:
    [INFO] [stderr] warning: simsimd@0.1.0: In file included from include/simsimd/binary.h:23:
    [INFO] [stderr] warning: simsimd@0.1.0: include/simsimd/types.h:119:9: error: _Float16 is not supported on this target
    [INFO] [stderr] warning: simsimd@0.1.0: typedef _Float16 simsimd_f16_t;
    [INFO] [stderr] warning: simsimd@0.1.0:         ^
    [INFO] [stderr] warning: simsimd@0.1.0: 1 error generated.
    [INFO] [stderr]
    [INFO] [stderr] error: failed to run custom build command for `simsimd v0.1.0 (/opt/rustwide/workdir)`
    [INFO] [stderr]
    [INFO] [stderr] Caused by:
    [INFO] [stderr]   process didn't exit successfully: `/opt/rustwide/target/debug/build/simsimd-bf9859d453372522/build-script-build`
    ```

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 fn main() {
+    // Docs.rs only target is x86_64
+    // `include/simsimd/types.h:119:9: error: _Float16 is not supported on this target`
+    if std::env::var("DOCS_RS").is_ok() {
+        return;
+    }
+
     cc::Build::new()
         .file("rust/lib.c")
         .include("include")
         .flag("-O3")
         .warnings(false)
+        .compiler("/usr/bin/clang")
         .compile("simsimd");
 
     println!("cargo:rerun-if-changed=rust/lib.c");
