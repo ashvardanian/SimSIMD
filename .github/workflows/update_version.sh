c15e5aa5c1f382fc855a42b582a96172ce76d1ba
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-01-28 03:25:55 +0000

    Make: Match only line starts in Cargo.toml

--- .github/workflows/update_version.sh
+++ .github/workflows/update_version.sh
@@ -4,6 +4,6 @@ echo $1 > VERSION &&
     sed -i "s/^\(#define SIMSIMD_VERSION_MAJOR \).*/\1$(echo "$1" | cut -d. -f1)/" include/simsimd/simsimd.h &&
     sed -i "s/^\(#define SIMSIMD_VERSION_MINOR \).*/\1$(echo "$1" | cut -d. -f2)/" include/simsimd/simsimd.h &&
     sed -i "s/^\(#define SIMSIMD_VERSION_PATCH \).*/\1$(echo "$1" | cut -d. -f3)/" include/simsimd/simsimd.h &&
-    sed -i "s/VERSION [0-9]\+\.[0-9]\+\.[0-9]\+/VERSION $1/" CMakeLists.txt &&
-    sed -i "s/version = \".*\"/version = \"$1\"/" Cargo.toml &&
-    sed -i "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json
+    sed -i "s/^version = \".*\"/version = \"$1\"/" Cargo.toml &&
+    sed -i "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json &&
+    sed -i "s/VERSION [0-9]\+\.[0-9]\+\.[0-9]\+/VERSION $1/" CMakeLists.txt
