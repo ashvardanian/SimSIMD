cdd282dc029d2e6c5323fcaceac73dc98f1f8154
Pedro Gabriel Planel <pplanel@users.noreply.github.com>
2024-01-30 00:46:02 -0300

    Fix: Rust cosine function (#77)
    * fix: fixed native cosine function
    
    * fix: cosine_similarity_cpu now returns a Option
    
    ---------
    
    Co-authored-by: Pedro Gabriel <pedro.planel@2gofintech.com.br>

--- rust/benches/native.rs
+++ rust/benches/native.rs
@@ -5,14 +5,16 @@ pub(crate) fn generate_random_vector(dim: usize) -> Vec<f32> {
     (0..dim).map(|_| rand::thread_rng().gen()).collect()
 }
 
-pub(crate) fn cosine_similarity_cpu(a: &[f32], b: &[f32]) -> f32 {
-    assert_eq!(a.len(), b.len());
+pub(crate) fn cosine_similarity_cpu(a: &[f32], b: &[f32]) -> Option<f32> {
+    if a.len() != b.len() {
+        return None;
+    }
 
     let dot_product: f32 = a.iter().zip(b.iter()).map(|(a, b)| a * b).sum();
     let norm_a: f32 = a.iter().map(|a| a.powf(2.0)).sum();
     let norm_b: f32 = b.iter().map(|b| b.powf(2.0)).sum();
 
-    dot_product / norm_a.sqrt() * norm_b.sqrt()
+    Some(1.0 - (dot_product / (norm_a.sqrt() * norm_b.sqrt())))
 }
 
 pub(crate) fn squared_euclidean_cpu(a: &[f32], b: &[f32]) -> f32 {
