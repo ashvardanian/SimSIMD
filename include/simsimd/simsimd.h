470ab82237cf7f0209e3c37c941ea11be351d439
TinySemVer <tinysemver@ashvardanian.com>
2024-10-06 22:05:58 +0000

    Release: v5.6.0 [skip ci]

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -87,8 +87,8 @@
 #define SIMSIMD_H
 
 #define SIMSIMD_VERSION_MAJOR 5
-#define SIMSIMD_VERSION_MINOR 5
-#define SIMSIMD_VERSION_PATCH 1
+#define SIMSIMD_VERSION_MINOR 6
+#define SIMSIMD_VERSION_PATCH 0
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
