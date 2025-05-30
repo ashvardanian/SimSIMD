eca39978366202b9e5b85acb27478bc37ee8fe18
TinySemVer <tinysemver@ashvardanian.com>
2025-05-30 14:08:23 +0000

    Release: v6.4.5 [skip ci] ### Patch
    - Fix: Aliasing of half-precision types (abb2d88)

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -90,7 +90,7 @@
 
 #define SIMSIMD_VERSION_MAJOR 6
 #define SIMSIMD_VERSION_MINOR 4
-#define SIMSIMD_VERSION_PATCH 4
+#define SIMSIMD_VERSION_PATCH 5
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
