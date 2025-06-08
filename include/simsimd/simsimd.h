9a4d32548b2f65dcdfef21df7a0d7caef848627f
TinySemVer <tinysemver@ashvardanian.com>
2025-06-08 03:22:00 +0000

    Release: v6.4.9 [skip ci] ### Patch
    - Fix: add dot i8 (Rust) (eaeb3b7)

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -90,7 +90,7 @@
 
 #define SIMSIMD_VERSION_MAJOR 6
 #define SIMSIMD_VERSION_MINOR 4
-#define SIMSIMD_VERSION_PATCH 8
+#define SIMSIMD_VERSION_PATCH 9
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
