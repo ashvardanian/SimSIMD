652eb7f77bb222be51d975acc1b17c61e96a621c
TinySemVer <tinysemver@ashvardanian.com>
2025-07-07 12:46:32 +0000

    Release: v6.5.0 [skip ci] ### Minor
    - Add: Half-precision converters for C/Rust (bf5a7d2)
    
    ### Patch
    
    - Improve: Support more numeric operators in Rust (1d7b2b8)
    - Improve: Rust `nostd` builds (c6c0698)
    - Make: Bump dependencies (3667313)
    - Fix: Expose `Distance` in Rust (e0c69c7)

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -88,8 +88,8 @@
 #define SIMSIMD_H
 
 #define SIMSIMD_VERSION_MAJOR 6
-#define SIMSIMD_VERSION_MINOR 4
-#define SIMSIMD_VERSION_PATCH 10
+#define SIMSIMD_VERSION_MINOR 5
+#define SIMSIMD_VERSION_PATCH 0
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
