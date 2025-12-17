6e843db07a01d8aaaf53b076c5a2a400790bcb78
TinySemVer <tinysemver@ashvardanian.com>
2025-12-17 06:37:27 +0000

    Release: v6.5.9 [skip ci] ### Patch
    - Make: Deno `--no-check` for CI (9976b88)
    - Fix: CMake relative paths for Termux compatibility (#288) (07976ad)
    - Make: NPM w/out `NODE_AUTH_TOKEN` (63cd55b)
    - Fix: Length check in `jaccard_b8_ice` and `hamming_b8_ice` (#286) (a7cc7e1)
    - Make: Python 3.14 builds (#271) (e4d62e7)
    - Fix: Avoid `sqrt(0)` in `probability.h` (108a8b5)
    - Fix: `u64size` in Rust to match the C ABI (31195e9)
    - Make: Stack-realign for `i386` builds (5a386f5)
    - Make: 32-bit cross-compilation for Rust (f473b2b)
    - Fix: Gating `core::fmt` in Rust (0709860)

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -91,7 +91,7 @@
 
 #define SIMSIMD_VERSION_MAJOR 6
 #define SIMSIMD_VERSION_MINOR 5
-#define SIMSIMD_VERSION_PATCH 8
+#define SIMSIMD_VERSION_PATCH 9
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
