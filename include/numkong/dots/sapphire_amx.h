bed477aa1929768be993256fefc6aea9fbdb5a6a
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2026-01-06 04:26:48 +0000

    Make: `amx-tile,amx-bf16,amx-int8` SPR flags

--- include/numkong/dots/sapphire_amx.h
+++ include/numkong/dots/sapphire_amx.h
@@ -42,9 +42,12 @@
 #if NK_TARGET_X86_
 #if NK_TARGET_SAPPHIRE_AMX
 #pragma GCC push_options
-#pragma GCC target("avx2", "avx512f", "avx512vl", "bmi2", "avx512bw", "avx512fp16")
-#pragma clang attribute push(__attribute__((target("avx2,avx512f,avx512vl,bmi2,avx512bw,avx512fp16"))), \
-                             apply_to = function)
+#pragma GCC target("avx2", "avx512f", "avx512vl", "avx512bw", "avx512dq", "avx512fp16", "f16c", "fma", "bmi", "bmi2", \
+                   "amx-tile", "amx-bf16", "amx-int8")
+#pragma clang attribute push(                                                                                        \
+    __attribute__((                                                                                                  \
+        target("avx2,avx512f,avx512vl,avx512bw,avx512dq,avx512fp16,f16c,fma,bmi,bmi2,amx-tile,amx-bf16,amx-int8"))), \
+    apply_to = function)
 
 #include "numkong/types.h"
 #include "numkong/cast/ice.h" // For FP8 ↔ BF16 conversions
