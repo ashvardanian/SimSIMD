b311d17ab9e13ebddb3a4ebda9d74a78109a276d
TinySemVer <tinysemver@ashvardanian.com>
2026-02-16 13:45:08 +0000

    Release: v6.5.13 [skip ci] ### Patch
    - Fix: Replace `avx2vnni` with `avxvnni` for Sierra Forest (#296) (a8bb232)
    - Make: Remove `NPM_TOKEN` for OIDC publishing (13cd5bc)
    - Make: Sign rebase with GitHub Actions bot (e7b89b5)
    - Fix: Revert to `atol=1` for test integer outputs vs SciPy (b75bdbd)

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -91,7 +91,7 @@
 
 #define SIMSIMD_VERSION_MAJOR 6
 #define SIMSIMD_VERSION_MINOR 5
-#define SIMSIMD_VERSION_PATCH 12
+#define SIMSIMD_VERSION_PATCH 13
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
