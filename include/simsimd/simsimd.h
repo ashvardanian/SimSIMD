746cea15bac73a778c0ef9a63f1b3f0e64e278c5
TinySemVer <tinysemver@ashvardanian.com>
2026-03-03 16:50:55 +0000

    Release: v6.5.14 [skip ci] ### Patch
    - Fix: Wrong predicate width in BF16 SVE L2 kernel (#301) (87ae846)
    - Improve: FreeBSD comp-time target selection (#300) (cb11f8b)

--- include/simsimd/simsimd.h
+++ include/simsimd/simsimd.h
@@ -91,7 +91,7 @@
 
 #define SIMSIMD_VERSION_MAJOR 6
 #define SIMSIMD_VERSION_MINOR 5
-#define SIMSIMD_VERSION_PATCH 13
+#define SIMSIMD_VERSION_PATCH 14
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
