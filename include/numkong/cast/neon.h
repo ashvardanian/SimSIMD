9ac3f5db68ec460cd18cf92b2dfa4281e8a6c37d
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2026-03-07 14:19:49 +0000

    Fix: MSVC ARM64 compatibility for NEON `f16`
    Replace vld1_f16/vst1_f16/vdupq_n_f16 with u16 load + reinterpret, as
    MSVC doesn't declare float16_t-typed NEON intrinsics without FP16 extension
    but the underlying instructions are base ARMv8-A

--- include/numkong/cast/neon.h
+++ include/numkong/cast/neon.h
@@ -67,7 +67,7 @@ extern "C" {
 #endif
 
 NK_PUBLIC void nk_f16_to_f32_neon(nk_f16_t const *src, nk_f32_t *dest) {
-    float16x4_t f16vec = vld1_dup_f16((nk_f16_for_arm_simd_t const *)src);
+    float16x4_t f16vec = vreinterpret_f16_u16(vld1_dup_u16((nk_u16_t const *)src));
     float32x4_t f32vec = vcvt_f32_f16(f16vec);
     *dest = vgetq_lane_f32(f32vec, 0);
 }
@@ -75,7 +75,7 @@ NK_PUBLIC void nk_f16_to_f32_neon(nk_f16_t const *src, nk_f32_t *dest) {
 NK_PUBLIC void nk_f32_to_f16_neon(nk_f32_t const *src, nk_f16_t *dest) {
     float32x4_t f32vec = vdupq_n_f32(*src);
     float16x4_t f16vec = vcvt_f16_f32(f32vec);
-    vst1_lane_f16((nk_f16_for_arm_simd_t *)dest, f16vec, 0);
+    vst1_lane_u16((nk_u16_t *)dest, vreinterpret_u16_f16(f16vec), 0);
 }
 
 #pragma region - Type Punned Loads and Stores
@@ -1006,7 +1006,7 @@ NK_PUBLIC void nk_cast_neon(void const *from, nk_dtype_t from_type, nk_size_t n,
             case nk_e5m2_k: hub_f16x8 = nk_e5m2x8_to_f16x8_neon_(vld1_u8(from_ptr)); break;
             case nk_e2m3_k: hub_f16x8 = nk_e2m3x8_to_f16x8_neon_(vld1_u8(from_ptr)); break;
             case nk_e3m2_k: hub_f16x8 = nk_e3m2x8_to_f16x8_neon_(vld1_u8(from_ptr)); break;
-            case nk_f16_k: hub_f16x8 = vld1q_f16((nk_f16_for_arm_simd_t const *)from_ptr); break;
+            case nk_f16_k: hub_f16x8 = vreinterpretq_f16_u16(vld1q_u16((nk_u16_t const *)from_ptr)); break;
             case nk_bf16_k: {
                 uint16x4_t bf16_lo = vld1_u16((nk_u16_t const *)from_ptr);
                 uint16x4_t bf16_hi = vld1_u16((nk_u16_t const *)(from_ptr + 8));
@@ -1014,14 +1014,14 @@ NK_PUBLIC void nk_cast_neon(void const *from, nk_dtype_t from_type, nk_size_t n,
                 float32x4_t f32_hi = nk_bf16x4_to_f32x4_neon_(bf16_hi);
                 hub_f16x8 = vcombine_f16(vcvt_f16_f32(f32_lo), vcvt_f16_f32(f32_hi));
             } break;
-            default: hub_f16x8 = vdupq_n_f16(0); break;
+            default: hub_f16x8 = vreinterpretq_f16_u16(vdupq_n_u16(0)); break;
             }
 
             // Downcast from f16x8 hub
             switch (to_type) {
             case nk_e4m3_k: vst1_u8(to_ptr, nk_f16x8_to_e4m3x8_neon_(hub_f16x8)); break;
             case nk_e5m2_k: vst1_u8(to_ptr, nk_f16x8_to_e5m2x8_neon_(hub_f16x8)); break;
-            case nk_f16_k: vst1q_f16((nk_f16_for_arm_simd_t *)to_ptr, hub_f16x8); break;
+            case nk_f16_k: vst1q_u16((nk_u16_t *)to_ptr, vreinterpretq_u16_f16(hub_f16x8)); break;
             case nk_bf16_k: {
                 float32x4_t f32_lo = vcvt_f32_f16(vget_low_f16(hub_f16x8));
                 float32x4_t f32_hi = vcvt_f32_f16(vget_high_f16(hub_f16x8));
@@ -1056,7 +1056,7 @@ NK_PUBLIC void nk_cast_neon(void const *from, nk_dtype_t from_type, nk_size_t n,
         float32x4_t hub_f32x4;
         switch (from_type) {
         case nk_f32_k: hub_f32x4 = vld1q_f32((nk_f32_t const *)from_ptr); break;
-        case nk_f16_k: hub_f32x4 = vcvt_f32_f16(vld1_f16((nk_f16_for_arm_simd_t const *)from_ptr)); break;
+        case nk_f16_k: hub_f32x4 = vcvt_f32_f16(vreinterpret_f16_u16(vld1_u16((nk_u16_t const *)from_ptr))); break;
         case nk_bf16_k: hub_f32x4 = nk_bf16x4_to_f32x4_neon_(vld1_u16((nk_u16_t const *)from_ptr)); break;
         case nk_e4m3_k: {
             nk_b32_vec_t in_vec;
@@ -1090,7 +1090,7 @@ NK_PUBLIC void nk_cast_neon(void const *from, nk_dtype_t from_type, nk_size_t n,
         // Downcast from f32x4 and store
         switch (to_type) {
         case nk_f32_k: vst1q_f32((nk_f32_t *)to_ptr, hub_f32x4); break;
-        case nk_f16_k: vst1_f16((nk_f16_for_arm_simd_t *)to_ptr, vcvt_f16_f32(hub_f32x4)); break;
+        case nk_f16_k: vst1_u16((nk_u16_t *)to_ptr, vreinterpret_u16_f16(vcvt_f16_f32(hub_f32x4))); break;
         case nk_bf16_k: vst1_u16((nk_u16_t *)to_ptr, nk_f32x4_to_bf16x4_neon_(hub_f32x4)); break;
         case nk_e4m3_k: {
             nk_b32_vec_t out_vec = nk_f32x4_to_e4m3x4_neon_(hub_f32x4);
