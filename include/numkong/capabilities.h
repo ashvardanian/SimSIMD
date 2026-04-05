c36030488e757bdb2925b16845824948894d5b4e
TinySemVer <tinysemver@ashvardanian.com>
2026-04-05 00:11:57 +0000

    Release: v7.4.1 [skip ci] ### Patch
    - Make: Set `repository.url` for NPM (385480d2)
    - Make: Pull MSVC ARM64 Cross-Compiler (e20c93ef)
    - Fix: Swap `f16x8` for `u16x8` in `cast_neon` (154ec5db)

--- include/numkong/capabilities.h
+++ include/numkong/capabilities.h
@@ -96,7 +96,7 @@
 
 #define NK_VERSION_MAJOR 7
 #define NK_VERSION_MINOR 4
-#define NK_VERSION_PATCH 0
+#define NK_VERSION_PATCH 1
 
 /**
  *  @brief  Removes compile-time dispatching, and replaces it with runtime dispatching.
